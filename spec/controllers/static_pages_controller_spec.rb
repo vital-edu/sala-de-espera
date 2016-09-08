c16d92d3c78685f5f5009b843b244fd6ef3d8e20
Eduardo Vital <vitaldu@gmail.com>
2016-09-08 12:18:09 -0300

    Setting up: test suite and code coverage
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+
+  context 'home action' do
+    it 'should home page be the root page' do
+      expect(get: root_url).to route_to(
+        controller: 'static_pages',
+        action: 'home'
+      )
+    end
+
+    it 'should render template home' do
+      get :home
+
+      expect(response).to render_template('home')
+    end
+  end
+
+end
