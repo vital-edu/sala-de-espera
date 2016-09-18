2b68fa49c7120772b3ab412823e733f28af5460a
Eduardo Vital <vitaldu@gmail.com>
2016-09-18 14:13:13 -0300

    Add client role to user by default
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :services
+  before_create :set_default_role
 
   rolify
   devise :database_authenticatable, :registerable, :timeoutable,
@@ -9,4 +10,10 @@ class User < ApplicationRecord
   validates_presence_of :name, :rg, :cpf
   validates_length_of :name, in: 1..100
   validates_confirmation_of :password
+
+  private
+
+  def set_default_role
+    self.add_role(:client) if self.roles.blank?
+  end
 end
