511efe852c732c57ba17e9b7356073d72d462cc1
Eduardo Vital <vitaldu@gmail.com>
2016-09-24 22:13:19 -0300

    Restricting acess to Service Category Controller
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- app/controllers/service_categories_controller.rb
+++ app/controllers/service_categories_controller.rb
@@ -1,5 +1,6 @@
 class ServiceCategoriesController < ApplicationController
   before_action :set_service_category, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   def index
     @service_categories = ServiceCategory.all
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,5 +2,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+    if user.has_role? :manager
+      can :manage, :all
+    end
   end
 end
