797c7ab8c23f8a97c26712d167885e855cf99066
Eduardo Vital <vitaldu@gmail.com>
2016-10-06 19:48:55 -0300

    Added list of Services into homepage
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     @images = Dir.glob("app/assets/images/carousel/*.{png,jpg,jpeg,gif}")
+    @service_category = ServiceCategory.all
   end
 end
