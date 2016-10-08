ca38800e782189448ca634b63f6eccdf59d5984b
Eduardo Vital <vitaldu@gmail.com>
2016-10-08 15:43:54 -0300

    Removing unecessary tests
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- spec/requests/service_categories_spec.rb
+++ spec/requests/service_categories_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "ServiceCategories", type: :request do
+
+  before(:each) do
+    allow_any_instance_of(CanCan::ControllerResource).to receive(:load_and_authorize_resource){ nil }
+  end
+
   describe "GET /service_categories" do
     it "works! (now write some real specs)" do
       get service_categories_path
--- spec/requests/services_spec.rb
+++ spec/requests/services_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Services", type: :request do
+
+  before(:each) do
+    allow_any_instance_of(CanCan::ControllerResource).to receive(:load_and_authorize_resource){ nil }
+  end
+
   describe "GET /services" do
     it "works! (now write some real specs)" do
+      allow_any_instance_of(ServicesController). to receive(:current_user) { build(:employee) }
       get services_path
       expect(response).to have_http_status(200)
     end
