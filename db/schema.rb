7cc12b99a349fccd537b384c4683bacde88987f3
Eduardo Vital <vitaldu@gmail.com>
2016-10-09 15:57:37 -0300

    Created seeds to ServiceCategory
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -19,10 +19,11 @@ ActiveRecord::Schema.define(version: 20160925000127) do
     t.string   "name"
     t.string   "resource_type"
     t.integer  "resource_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
     t.index ["name"], name: "index_roles_on_name", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id", using: :btree
   end
 
   create_table "service_categories", force: :cascade do |t|
@@ -84,7 +85,9 @@ ActiveRecord::Schema.define(version: 20160925000127) do
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id", using: :btree
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
+    t.index ["user_id"], name: "index_users_roles_on_user_id", using: :btree
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,33 @@
 Role.create(name: 'manager')
 Role.create(name: 'employee')
 Role.create(name: 'client')
+
+ServiceCategory.create(
+  name: "Corte de Cabelo",
+  description: "Corte de cabelo masculino e feminino.",
+  price: 20,
+  image: File.new('app/assets/images/service_categories/haircut.jpg'))
+
+ServiceCategory.create(
+  name: "Manicure",
+  description: "Manicure.",
+  price: 15,
+  image: File.new('app/assets/images/service_categories/manicure.jpg'))
+
+ServiceCategory.create(
+  name: "Pedicure",
+  description: "Pedicure.",
+  price: 15,
+  image: File.new('app/assets/images/service_categories/pedicure.jpg'))
+
+ServiceCategory.create(
+  name: "Escova Progressiva",
+  description: "Escova progressiva feminina.",
+  price: 80,
+  image: File.new('app/assets/images/service_categories/progressive_brush.jpg'))
+
+ServiceCategory.create(
+  name: "Penteado feminino",
+  description: "Penteado feminino para casamentos e outros eventos importantes.",
+  price: 60,
+  image: File.new('app/assets/images/service_categories/hairstyle.jpg'))
